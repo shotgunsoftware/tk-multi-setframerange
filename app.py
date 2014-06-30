d68ffab1c446f8ed2c4aae109bc2c2e585af10c5
Manne Öhrström <manne@shotgunsoftware.com>
2014-06-30 13:30:53 +0100

    Added softimage render range support. Better handling of string fields. Closes #7

--- app.py
+++ app.py
@@ -53,10 +53,10 @@ class SetFrameRange(Application):
             # change!
             message =  "Your scene has been updated with the \n"
             message += "latest frame ranges from shotgun.\n\n"
-            message += "Previous start frame: %d\n" % current_in
-            message += "New start frame: %d\n\n" % new_in
-            message += "Previous end frame: %d\n" % current_out
-            message += "New end frame: %d\n\n" % new_out
+            message += "Previous start frame: %s\n" % current_in
+            message += "New start frame: %s\n\n" % new_in
+            message += "Previous end frame: %s\n" % current_out
+            message += "New end frame: %s\n\n" % new_out
             self.set_frame_range(self.engine.name, new_in, new_out)
 
         else:
@@ -194,8 +194,14 @@ class SetFrameRange(Application):
             import win32com
             Application = win32com.client.Dispatch('XSI.Application')
 
+            # set playback control
             Application.SetValue("PlayControl.In", in_frame)
             Application.SetValue("PlayControl.Out", out_frame)
+            
+            # set frame ranges for rendering
+            Application.SetValue("Passes.RenderOptions.FrameStart", in_frame)
+            Application.SetValue("Passes.RenderOptions.FrameEnd", out_frame)
+            
 
         elif engine == "tk-houdini":
             import hou
