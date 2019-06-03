27c5d911904c168dd9ff80ab081d63ef7374fa92
Unai Martínez Barredo <unai.barredo@jellyfishpictures.co.uk>
2019-06-03 10:28:29 +0100

    Removed use of PyMEL

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # Copyright (c) 2013 Shotgun Software Inc.
-# 
+#
 # CONFIDENTIAL AND PROPRIETARY
-# 
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
 # Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your 
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 """
@@ -63,20 +63,20 @@ class SetFrameRange(Application):
             message += "in and out frame data for this Shot."
             QtGui.QMessageBox.information(None, "No data in Shotgun!", message)
             return
-            
+
         # now update the frame range.
         # because the frame range is often set in multiple places (e.g render range,
         # current range, anim range etc), we go ahead an update every time, even if
         # the values in Shotgun are the same as the values reported via get_current_frame_range()
         self.set_frame_range(self.engine.name, new_in, new_out)
-        
+
         message =  "Your scene has been updated with the \n"
         message += "latest frame ranges from shotgun.\n\n"
         message += "Previous start frame: %s\n" % current_in
         message += "New start frame: %s\n\n" % new_in
         message += "Previous end frame: %s\n" % current_out
         message += "New end frame: %s\n\n" % new_out
-        
+
         QtGui.QMessageBox.information(None, "Frame range updated!", message)
 
 
@@ -119,7 +119,6 @@ class SetFrameRange(Application):
     def get_current_frame_range(self, engine):
 
         if engine == "tk-maya":
-            import pymel.core as pm
             import maya.cmds as cmds
             current_in = cmds.playbackOptions(query=True, minTime=True)
             current_out = cmds.playbackOptions(query=True, maxTime=True)
@@ -165,19 +164,18 @@ class SetFrameRange(Application):
     def set_frame_range(self, engine, in_frame, out_frame):
 
         if engine == "tk-maya":
-            import pymel.core as pm
-            
-            # set frame ranges for plackback
-            pm.playbackOptions(minTime=in_frame, 
-                               maxTime=out_frame,
-                               animationStartTime=in_frame,
-                               animationEndTime=out_frame)
-            
+            import maya.cmds as cmds
+
+            # set frame ranges for playback
+            cmds.playbackOptions(minTime=in_frame,
+                                 maxTime=out_frame,
+                                 animationStartTime=in_frame,
+                                 animationEndTime=out_frame)
+
             # set frame ranges for rendering
-            defaultRenderGlobals=pm.PyNode('defaultRenderGlobals')
-            defaultRenderGlobals.startFrame.set(in_frame)
-            defaultRenderGlobals.endFrame.set(out_frame)
-           
+            cmds.setAttr('defaultRenderGlobals.startFrame', in_frame)
+            cmds.setAttr('defaultRenderGlobals.endFrame', out_frame)
+
         elif engine == "tk-nuke" and not self.engine.hiero_enabled:
             import nuke
 
@@ -202,32 +200,32 @@ class SetFrameRange(Application):
         elif engine == "tk-softimage":
             import win32com
             Application = win32com.client.Dispatch('XSI.Application')
-            
+
             # set playback control
             Application.SetValue("PlayControl.In", in_frame)
             Application.SetValue("PlayControl.Out", out_frame)
             Application.SetValue("PlayControl.GlobalIn", in_frame)
-            Application.SetValue("PlayControl.GlobalOut", out_frame)       
-            
+            Application.SetValue("PlayControl.GlobalOut", out_frame)
+
             # set frame ranges for rendering
             Application.SetValue("Passes.RenderOptions.FrameStart", in_frame)
             Application.SetValue("Passes.RenderOptions.FrameEnd", out_frame)
-            
+
 
         elif engine == "tk-houdini":
             import hou
             # We have to use hscript until SideFX gets around to implementing hou.setGlobalFrameRange()
-            hou.hscript("tset `((%s-1)/$FPS)` `(%s/$FPS)`" % (in_frame, out_frame))            
+            hou.hscript("tset `((%s-1)/$FPS)` `(%s/$FPS)`" % (in_frame, out_frame))
             hou.playbar.setPlaybackRange(in_frame, out_frame)
 
         elif engine == "tk-3dsmax":
             from Py3dsMax import mxs
             mxs.animationRange = mxs.interval(in_frame, out_frame)
         elif engine == "tk-3dsmaxplus":
-            import MaxPlus 
+            import MaxPlus
             ticks = MaxPlus.Core.EvalMAXScript("ticksperframe").GetInt()
             range = MaxPlus.Interval(in_frame * ticks, out_frame * ticks)
             MaxPlus.Animation.SetRange(range)
-        
+
         else:
             raise tank.TankError("Don't know how to set current frame range for engine %s!" % engine)
